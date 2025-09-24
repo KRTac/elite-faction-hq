da49aca66bae1da61983b8879b23c496033c7dce
Dino Krtanjek <krtanjekdino@gmail.com>
2025-09-24 15:15:21 +0200

    Removed directory list from rollup external

---
+++ src/routes/contact.lazy.jsx
@@ -0,0 +1,39 @@
+import { Link, createLazyFileRoute } from '@tanstack/react-router';
+import StandardLayout from '../components/layouts/Standard';
+
+import { factions } from '../assets/factions_meta.json';
+
+
+function Dashboard() {
+  return (
+    <StandardLayout>
+      <div
+        className={[
+          'max-w-3xl mx-auto mt-3 p-3 gap-3',
+          'flex flex-wrap justify-center'
+        ].join(' ')}
+      >
+        {factions.map(faction => {
+          return (
+            <Link
+              className={[
+                'block px-6 py-4 rounded-md dark:bg-bg2-d dark:hover:bg-bg2-d/80',
+                'w-full max-w-1/2 cursor-pointer transition duration-200',
+                'border-2 border-transparent dark:hover:border-neutral-600',
+                'text-lg dark:hover:text-accent-d no-underline'
+              ].join(' ')}
+              to={`/${faction.directory}`}
+              key={faction.directory}
+            >
+              {faction.name}
+            </Link>
+          );
+        })}
+      </div>
+    </StandardLayout>
+  );
+}
+
+export const Route = createLazyFileRoute('/contact')({
+  component: Dashboard
+});
